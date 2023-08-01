bb604174bb57962d7cb24e50ebfdbc972896da59
Renan Rosa <renan.rosa@labldi.com.br>
2023-07-31 21:20:23 -0300

    Some styling applied

---
+++ README.md
@@ -0,0 +1,3 @@
+# Html-in-Action
+
+This repository contains the source code from the book Html in Action with some improvements.
---
+++ files/app.js
@@ -0,0 +1,58 @@
+class SuperEditor {
+
+    constructor() {
+        this.createForm = document.forms.create
+        this.importForm = document.forms.import
+
+        this.view, this.fileName, this.isDirty = false;
+        this.unsavedMsg = 'Unsaved changes will be lost. Are you sure?',
+            this.unsavedTitle = 'Discard changes';
+    }
+
+    markDirty() {
+        this.isDirty = true;
+    };
+
+    markClean() {
+        this.isDirty = false;
+    };
+
+    checkDirty() {
+        if (this.isDirty) { return unsavedMsg; }
+    };
+
+    jump(e) {
+        let hash = location.hash;
+        if (hash.indexOf('/') > -1) {
+            let parts = hash.split('/');
+            let fileNameEl = document.getElementById('file_name');
+            view = parts[0].substring(1) + '-view';
+
+            fileName = parts[1];
+            fileNameEl.innerHTML = fileName;
+        } else {
+            if (!this.isDirty || confirm(unsavedMsg, unsavedTitle)) {
+                this.markClean();
+                view = 'browser-view';
+                if (hash != '#list') {
+                    location.hash = '#list';
+                }
+            } else {
+                location.href = e.oldURL;
+            }
+        }
+        document.body.className = view;
+    };
+
+}
+
+window.onload = function () {
+
+    let editor = new SuperEditor()
+
+    window.addEventListener('beforeunload', editor.checkDirty.bind(editor), false);
+    window.addEventListener('hashchange', editor.jump.bind(editor), false);
+
+};
+
+
